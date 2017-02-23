4e3ed57a40be70e021455b2255f647344c411a92
Jeremy Petrotto <jmpetrot@buffalo.edu>
2017-02-23 13:28:54 -0500

    Added working keyboard

--- UBTalker/Views/KeyboardView.xaml.cs
+++ UBTalker/Views/KeyboardView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Practices.ServiceLocation;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using UBTalker.Controls;
 
 namespace UBTalker.Views
 {
@@ -10,20 +12,78 @@ namespace UBTalker.Views
     public partial class KeyboardView : Page
     {
         private readonly IKeyboardReceiver _parent = ServiceLocator.Current.GetInstance<IMainWindow>();
+        private List<GazeButtonData[]> Alphabet_Menu;
+        private List<GazeButtonData[]> Symbols_Menu;
+        private List<GazeButtonData[]> Menu = new List<GazeButtonData[]> { new GazeButtonData[] {
+            new GazeButtonData("A-Z", "cmd:alphabet"),
+            new GazeButtonData("0-9, .,!?_", "cmd:symbols"),
+            new GazeButtonData("Ok", "cmd:ok"),
+            new GazeButtonData("Cancel", "cmd:cancel")
+        } };
 
         public KeyboardView()
         {
             InitializeComponent();
+
+            Alphabet_Menu = new List<GazeButtonData[]>();
+            Alphabet_Menu.Add(GazeButtonData.FromStringArray(new string[] { "a", "b", "c", "d" }));
+            Alphabet_Menu.Add(GazeButtonData.FromStringArray(new string[] { "e", "f", "g", "h" }));
+            Alphabet_Menu.Add(GazeButtonData.FromStringArray(new string[] { "i", "j", "k", "l" }));
+            Alphabet_Menu.Add(GazeButtonData.FromStringArray(new string[] { "m", "n", "o", "p" }));
+            Alphabet_Menu.Add(GazeButtonData.FromStringArray(new string[] { "q", "r", "s", "t" }));
+            Alphabet_Menu.Add(GazeButtonData.FromStringArray(new string[] { "u", "v", "w", "x" }));
+            Alphabet_Menu.Add(GazeButtonData.FromStringArray(new string[] { "y", "z", "", "" }));
+
+            Symbols_Menu = new List<GazeButtonData[]>();
+            Symbols_Menu.Add(GazeButtonData.FromStringArray(new string[] { "0", "1", "2", "3" }));
+            Symbols_Menu.Add(GazeButtonData.FromStringArray(new string[] { "4", "5", "6", "7" }));
+            Symbols_Menu.Add(GazeButtonData.FromStringArray(new string[] { "8", "9", ".", "," }));
+            Symbols_Menu.Add(GazeButtonData.FromStringArray(new string[] { "!", "?", "(space)", "(backspace)" }));
+
+            PagedMenu.Options = Menu;
         }
 
-        private void GazeButton_GazeSelect_Cancel(object sender, RoutedEventArgs e)
+        private void PagedGazeMenu_GazeSelect(object sender, RoutedEventArgs e)
         {
-            _parent.OnKeyboardCancel();
-        }
+            if (e.OriginalSource is GazeButton)
+            {
+                var button = e.OriginalSource as GazeButton;
 
-        private void GazeButton_GazeSelect_OK(object sender, RoutedEventArgs e)
-        {
-            _parent.OnKeyboardInput("OK");
+                if (button.Link != null)
+                {
+                    switch (button.Link)
+                    {
+                        case "cmd:alphabet":
+                            PagedMenu.Options = Alphabet_Menu;
+                            break;
+                        case "cmd:symbols":
+                            PagedMenu.Options = Symbols_Menu;
+                            break;
+                        case "cmd:ok":
+                            _parent.OnKeyboardInput(CurrentText.Text);
+                            break;
+                        case "cmd:cancel":
+                            _parent.OnKeyboardCancel();
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (button.Text)
+                    {
+                        case "(space)":
+                            CurrentText.Text += " ";
+                            break;
+                        case "(backspace)":
+                            CurrentText.Text = CurrentText.Text.Remove(CurrentText.Text.Length - 1, 1);
+                            break;
+                        default:
+                            CurrentText.Text += button.Text;
+                            break;
+                    }
+                    PagedMenu.Options = Menu;
+                }
+            }
         }
     }
 }
