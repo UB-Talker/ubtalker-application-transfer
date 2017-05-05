b7bf305aa06c8e9abd57f8aff34c16f08cee4c0a
jhoeber <jrhoeber@buffalo.edu>
2017-05-04 18:01:56 -0700

    Uncommented eye tracker init

--- UBTalker/App.xaml.cs
+++ UBTalker/App.xaml.cs
@@ -31,7 +31,7 @@ namespace UBTalker
 
             // Start EyeXHost
             _eyeXHost = new WpfEyeXHost();
-            //_eyeXHost.Start();
+            _eyeXHost.Start();
 
             // Setup Unity Container
             _container = new UnityContainer();
